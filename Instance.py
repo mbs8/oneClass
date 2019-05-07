6049643607c1202578e9986e121e81251e626637
Branco <mbs8@cin.ufpe.br>
2019-05-07 09:00:38 -0300

    Uploads the datasets and the code of the instance class

---
+++ Instance.py
@@ -0,0 +1,60 @@
+import math
+import operator
+
+def alpha(actualIndex, totalIndex):
+    return 0.5 * (1 - ((actualIndex)/ totalIndex))
+
+class Instance:
+    def __init__(self, id, params, classification):
+        self.id = id
+        self.params = params
+        self.classification = classification
+        self.distancesToInstances = []
+
+    # Calcula a distancia para determinada instancia usando os vetores de min e max 
+    # para a normalização do cálculo da distância
+    def euclideanDistance(self, datasetInstance, minArg, maxArg):
+        distance = 0
+        for i, param in enumerate(datasetInstance.params):
+            distance += ((param - self.params[i]) ** 2) / (maxArg[i] - minArg[i])
+        distance = math.sqrt(distance)       
+        return distance
+    
+    # Insere ordenadamente no array de distancias a distancia para determinada instancia
+    def insertDistance(self, distanceToInstance, instance, k): 
+        distInst = (distanceToInstance, instance)
+        if self.distancesToInstances == []:
+            self.distancesToInstances.append(distInst)
+        else:
+            for i, (dist, _) in enumerate(self.distancesToInstances):
+                if dist > distanceToInstance:
+                    self.distancesToInstances.insert(i, distInst)
+                    size = len(self.distancesToInstances)
+                    if size > k:
+                        del(self.distancesToInstances[size-1])
+                    return
+            if len(self.distancesToInstances) < k:
+                self.distancesToInstances.append(distInst)
+                
+    
+    # Retorna a classe pertencente da instancia
+    def classify(self, numNeighbor, classes):
+        dictClass = {}
+
+        for clas in classes:
+            dictClass[clas] = 0
+
+        for i in range(0, numNeighbor):
+            classification = self.distancesToInstances[i][1].classification
+            dictClass[classification] += 1
+
+        return max(dictClass.items(), key=operator.itemgetter(1))[0]
+
+    def adjustParam(self, neighbor, aprox, indAtual, totalInd):
+        i = 0
+        while i < len(self.params):
+            if aprox:
+                self.params[i] += alpha(indAtual, totalInd) * (neighbor.params[i] - self.params[i])
+            else:
+                self.params[i] -= alpha(indAtual, totalInd) * (neighbor.params[i] - self.params[i])
+            i += 1
